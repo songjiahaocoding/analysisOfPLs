7a69804be13ab6830413cc519bab32f10eff8a34
Mars <marssong66@gmail.com>
2023-04-14 14:55:44 -0700

    DEBUG: Didn't read the EOL char. Add a judge at the end of each line to resolve it.

--- week_2/four.java
+++ week_2/four.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 
 public class four {
     static HashSet<String> stopSet = new HashSet<>();
+
     public static void main(String[]args) throws IOException {
         // Read stop words
         InputStream is = new FileInputStream("../stop_words.txt");
@@ -16,38 +17,42 @@ public class four {
         for (String s:stopWords.split(",")){
             stopSet.add(s);
         }
+        for (int i=0;i<26;i++){
+            char c = (char) ('a'+i);
+            stopSet.add(String.valueOf(c));
+        }
 
-        for (String s: args) System.out.println(s);
         // Read the content of the novel
         FileInputStream fis = new FileInputStream(args[0]);
         BufferedReader br = new BufferedReader(new InputStreamReader(fis));
-        System.out.println("Buffer");
         ArrayList<Pair<String, Integer>> freqs = new ArrayList<>();
         String content = null;
         int cnt = 0;
         while((content = br.readLine())!=null){
-            System.out.println(cnt);
+            cnt++;
             int start = -1;
             int i = 0;
             for(char c : content.toCharArray()){
                 if (start==-1){
-                    if (Character.isLowerCase(c)||Character.isUpperCase(c)){
+                    if (Character.isLetter(c) || Character.isDigit(c)){
                         start = i;
                     }
                 } else {
-                    if (!Character.isLowerCase(c) && !Character.isUpperCase(c)){
+                    if (!Character.isLetter(c) && !Character.isDigit(c)){
                         String word = content.substring(start, i).toLowerCase();
                         if(!stopSet.contains(word)){
                             int index = 0;
-                            for (;index<freqs.size();i++){
-                                if (freqs.get(index).getKey().equals(word)){
+                            for (;index<freqs.size();index++){
+                                Pair<String, Integer> pair = freqs.get(index);
+                                if (pair.getKey().equals(word)){
+                                    freqs.set(index, new Pair<>(word, pair.getValue()+1));
                                     break;
                                 }
                             }
                             if (index>=freqs.size()){
                                 freqs.add(new Pair<>(word, 1));
                             } else if (freqs.size()>1) {
-                                for (int j=index;j>=0;j--){
+                                for (int j=index-1;j>=0;j--){
                                     if (freqs.get(index).getValue() > freqs.get(j).getValue()){
                                         Pair<String, Integer> pair = freqs.get(j);
                                         freqs.set(j, freqs.get(index));
@@ -63,7 +68,31 @@ public class four {
                 }
                 i++;
             }
-            cnt++;
+            if (start!=-1){
+                String word = content.substring(start, i).toLowerCase();
+                if(!stopSet.contains(word)){
+                    int index = 0;
+                    for (;index<freqs.size();index++){
+                        Pair<String, Integer> pair = freqs.get(index);
+                        if (pair.getKey().equals(word)){
+                            freqs.set(index, new Pair<>(word, pair.getValue()+1));
+                            break;
+                        }
+                    }
+                    if (index>=freqs.size()){
+                        freqs.add(new Pair<>(word, 1));
+                    } else if (freqs.size()>1) {
+                        for (int j=index-1;j>=0;j--){
+                            if (freqs.get(index).getValue() > freqs.get(j).getValue()){
+                                Pair<String, Integer> pair = freqs.get(j);
+                                freqs.set(j, freqs.get(index));
+                                freqs.set(index, pair);
+                                index = j;
+                            }
+                        }
+                    }
+                }
+            }
         }
 
         for(int i=0;i<25;i++){
